a43d2562c12e17098f9dc75e12430ece83a4a0aa
Kingyeung129 <kingyeung129@hotmail.com>
2024-12-29 00:48:19 +0800

    Modify server response

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, this is your staging environment!');
+    res.send('Hello, this is your staging environment! For CI/CD github actions testing purposes...');
 });
 
 app.listen(port, () => {
